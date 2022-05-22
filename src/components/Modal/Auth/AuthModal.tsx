7088adaa627408e2173a9d32b049fae4f96a1e17
Nidhi Elango <nidhielango3@gmail.com>
2022-05-22 17:57:53 -0400

    modal setup

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,15 +1,21 @@
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
 import React from 'react';
-
+import { authModalState } from '../../../atoms/authModalAtom';
+import { useRecoilState } from 'recoil';
 
 const AuthModal:React.FC = () => {
-    
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+      setModalState((prev) => ({
+          ...prev, 
+          open: false,
+      }));
+  };
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Modal Title</ModalHeader>
@@ -17,15 +23,10 @@ const AuthModal:React.FC = () => {
           <ModalBody>
             MODAL BODY
           </ModalBody>
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
   )
 }
 export default AuthModal;
+
--- src/components/Navbar/Content/AuthButtons.tsx
+++ src/components/Navbar/Content/AuthButtons.tsx
@@ -1,14 +1,21 @@
 import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { authModalState } from '../../../atoms/authModalAtom';
 
 const AuthButtons:React.FC = () => {
+    const setAuthModalState = useSetRecoilState(authModalState);
     
     return (
         <>
         <Button variant="outline" height="30px" display={{base:"none", sm:"flex"}}
-        width={{base:"70px", md:"110px"}} mr={2}>Log In</Button>
+        width={{base:"70px", md:"110px"}} mr={2}
+        onClick={() => setAuthModalState({open: true, view: "login"})}>
+            Log In</Button>
         <Button height="30px" display={{base:"none", sm:"flex"}}
-        width={{base:"70px", md:"110px"}} mr={2}>Sign In</Button>
+        width={{base:"70px", md:"110px"}} mr={2}
+        onClick={() => setAuthModalState({open: true, view: "signup"})}>
+            Sign In</Button>
         </>
     )
 }
