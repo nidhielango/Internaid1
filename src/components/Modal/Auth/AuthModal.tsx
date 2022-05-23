9854ff41a23ced687ed1411241e7b35b689bf3be
Nidhi Elango <nidhielango3@gmail.com>
2022-05-22 21:54:41 -0400

    oauth button

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,8 +1,9 @@
-import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex } from '@chakra-ui/react';
+import { Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { authModalState } from '../../../atoms/authModalAtom';
 import { useRecoilState } from 'recoil';
 import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 const AuthModal:React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -28,6 +29,8 @@ const AuthModal:React.FC = () => {
           <ModalBody display="flex" flexDirection="column" alignItems="center" justifyContent="center" pb={5}>
 
             <Flex direction="column" align="center" justify="center" width="70%">
+               <OAuthButtons/>
+               <Text color="gray.600" fontWeight={700}>OR</Text>
                <AuthInputs />
             </Flex>
          
