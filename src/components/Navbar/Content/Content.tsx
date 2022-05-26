437816dfb15a71de2bb081f710b3d2a37a705499
Nidhi Elango <nidhielango3@gmail.com>
2022-05-26 12:24:05 -0400

    menu

--- src/components/Navbar/Content/Content.tsx
+++ src/components/Navbar/Content/Content.tsx
@@ -1,10 +1,11 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex} from '@chakra-ui/react';
 import { signOut, User } from 'firebase/auth';
 import React from 'react';
 import { auth } from '../../../firebase/clientApp';
 import AuthModal from '../../Modal/Auth/AuthModal';
 import AuthButtons from './AuthButtons';
 import Icons from './Icons';
+import NavMenu from "./NavMenu";
 
 type ContentProps = {
     user?: User| null; 
@@ -17,6 +18,7 @@ const Content:React.FC<ContentProps> = ({user}) => {
         <AuthModal />
         <Flex justify="center" align="center">
            {user ? <Icons/> : <AuthButtons/>}
+           <NavMenu user={user}/>
         </Flex>
         </>
         
