1123172a0d4216e6d1f5b5ea0956fe32b33c00fd
Nidhi Elango <nidhielango3@gmail.com>
2022-05-27 17:37:34 -0400

    navbar company component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/clientApp';
 import Content from './Content/Content';
+import Directory from './Directory/Directory';
 import Search from './Search';
 
 const Navbar:React.FC = () => {
@@ -11,11 +12,13 @@ const Navbar:React.FC = () => {
     //  <Image src="/images/logo.png" height="40px"/> Logo
     //  <Image src="/images/logoText.png" height="45px" display={{base: 'none', md: "unset"}}/> Text Image
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-           <Flex align="center">
+        <Flex bg="white" height="44px" padding="6px 12px" justify={{md:"space-between"}}>
+           <Flex align="center" width={{base: "40px", md: "auto"}} mr={{base:0, md:2}}>
 
            </Flex>
-        <Search/>
+           {/* Directory is present only when user logs in */}
+           {user && <Directory />} 
+        <Search user={user}/>
         <Content user={user}/>
         </Flex>
     )
--- src/components/Navbar/Search.tsx
+++ src/components/Navbar/Search.tsx
@@ -1,15 +1,16 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import React from 'react';
 
 type SearchProps = {
-  
+    user?: User| null;
 };
 
-const Search:React.FC<SearchProps> = () => {
+const Search:React.FC<SearchProps> = ({user}) => {
     
     return (
-        <Flex flexGrow={1} mr={2} align="center"> 
+        <Flex flexGrow={1} mr={2} maxWidth={user? "auto": "600px"} align="center"> 
             <InputGroup>
             <InputLeftElement 
                 pointerEvents="none"
