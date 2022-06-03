6175ead3464b25cbf0b60f032b7c4d9a4ac4aa25
Nidhi Elango <nidhielango3@gmail.com>
2022-06-02 23:50:30 -0400

    company data

--- src/atoms/companiesAtom.ts
+++ src/atoms/companiesAtom.ts
@@ -9,4 +9,44 @@ export interface Company {
     privacyType: "public" | "restrictied" | "private";
     createdAt?: Timestamp;
     imageURL?: string;
-}
+} 
+
+export interface CompanySnippet {
+    companyId: string;
+    isModerator?: boolean;
+    imageURL?: string;
+  }
+  
+  interface CompanyState {
+    [key: string]:
+      | CompanySnippet[]
+      | { [key: string]: Company}
+      | Company
+      | boolean
+      | undefined;
+    mySnippets: CompanySnippet[];
+    initSnippetsFetched: boolean;
+    visitedCommunities: {
+      [key: string]: Company;
+    };
+    currentCommunity: Company;
+  }
+  
+  export const defaultCompany: Company = {
+    id: "",
+    creatorId: "",
+    numberOfMembers: 0,
+    privacyType: "public",
+  };
+  
+  export const defaultCompanyState: CompanyState = {
+    mySnippets: [],
+    initSnippetsFetched: false,
+    visitedCommunities: {},
+    currentCommunity: defaultCompany,
+  };
+  
+  export const companyState = atom<CompanyState>({
+    key: "companyState",
+    default: defaultCompanyState,
+  });
