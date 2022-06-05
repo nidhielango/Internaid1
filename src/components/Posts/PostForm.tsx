a1acb3584e2cb0df54109703e1003a5faeea9304
Nidhi Elango <nidhielango3@gmail.com>
2022-06-05 18:13:12 -0400

    post form

--- src/components/Posts/PostForm.tsx
+++ src/components/Posts/PostForm.tsx
@@ -3,10 +3,11 @@ import React, { useState } from 'react';
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
 import { BiPoll} from 'react-icons/bi';
 import TabItem from './TabItem';
+import TextInputs from './Form/TextInputs';
 
 type PostFormProps = { };
 
-const Tabs = [
+const Tabs: TabItem[]= [
     {
       title: "Post",
       icon: IoDocumentText,
@@ -21,13 +22,30 @@ const Tabs = [
     },
   ];
 
-  export type TabItem = {
+export type TabItem = {
     title: string;
     icon: typeof Icon.arguments;
   };
 
 const PostForm:React.FC<PostFormProps> = () => {
     const [selectedTab, setSelectedTab] = useState(Tabs[0].title);
+    const [textInputs, setTextInputs] = useState({
+      title: "",
+      body: "",
+    });
+    const [selectedFile, setSelectedFile] = useState<string>();
+    const handleCreatePost = async() => {};
+    const onSelectImage = () => {};
+    const [loading, setLoading] = useState(false);
+    const onTextChange = ({
+      target: { name, value },
+    }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setTextInputs((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    };
+
     return (
         <Flex direction="column" bg="white" borderRadius={4} mt={2}>
             <Flex width="100%">
@@ -35,6 +53,17 @@ const PostForm:React.FC<PostFormProps> = () => {
                     <TabItem item={item} selected={item.title ===selectedTab} setSelectedTab={setSelectedTab}/>
                 ))}
             </Flex>
+            <Flex p={3}>
+            {selectedTab === "Post" && (
+              <TextInputs
+                textInputs={textInputs}
+                onChange={onTextChange}
+                handleCreatePost={handleCreatePost}
+                loading={loading}
+              />
+            )}
+            
+          </Flex>
         </Flex>
     )
 }
